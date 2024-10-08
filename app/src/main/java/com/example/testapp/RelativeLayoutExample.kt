e72e3c6c1d87d3502b7aa3c3fbc2259afd4f9699
agsjohn <joaovitormds03@gmail.com>
2024-10-08 16:09:15 -0300

    segundo commit

--- app/src/main/java/com/example/testapp/RelativeLayoutExample.kt
+++ app/src/main/java/com/example/testapp/RelativeLayoutExample.kt
@@ -1,7 +1,13 @@
 package com.example.testapp
 
+import android.R
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updateLayoutParams
 import com.example.testapp.databinding.RelativeLayoutExampleBinding
 
 class RelativeLayoutExample: AppCompatActivity() {
@@ -13,5 +19,19 @@ class RelativeLayoutExample: AppCompatActivity() {
 
         binding = RelativeLayoutExampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val botao = binding.button1
+        val textView = binding.textView1
+        var cont = 0
+        botao.setOnClickListener(){
+            val params = textView.layoutParams as RelativeLayout.LayoutParams
+            if(cont.mod(2).equals(0)) {
+                params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE)
+            } else{
+                params.removeRule(RelativeLayout.ALIGN_PARENT_TOP)
+            }
+            cont++
+            textView.layoutParams = params
+        }
     }
 }
--- app/src/main/java/com/example/testapp/ScrollViewExample.kt
+++ app/src/main/java/com/example/testapp/ScrollViewExample.kt
@@ -1,5 +1,6 @@
 package com.example.testapp
 
+import android.R.string
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testapp.databinding.ScrollviewExampleBinding
@@ -13,5 +14,33 @@ class ScrollViewExample: AppCompatActivity() {
 
         binding = ScrollviewExampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button1.setOnClickListener(){
+            binding.text1.setText(R.string.LoremIpsum)
+        }
+        binding.button2.setOnClickListener(){
+            binding.text2.setText(R.string.LoremIpsum)
+        }
+        binding.button3.setOnClickListener(){
+            binding.text3.setText(R.string.LoremIpsum)
+        }
+        binding.button4.setOnClickListener(){
+            binding.text4.setText(R.string.LoremIpsum)
+        }
+        binding.button5.setOnClickListener(){
+            binding.text5.setText(R.string.LoremIpsum)
+        }
+        binding.button6.setOnClickListener(){
+            binding.text6.setText(R.string.LoremIpsum)
+        }
+        binding.button7.setOnClickListener(){
+            binding.text7.setText(R.string.LoremIpsum)
+        }
+        binding.button8.setOnClickListener(){
+            binding.text8.setText(R.string.LoremIpsum)
+        }
+        binding.button9.setOnClickListener(){
+            binding.text9.setText(R.string.LoremIpsum)
+        }
     }
 }
